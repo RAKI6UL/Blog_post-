08460669f7cf7e0c8950e58d7f562a6fe0e554c0
RAKI6Ul <numanislam3131@gmail.com>
2023-10-19 14:55:14 +0600

    update

--- Blog_site/settings.py
+++ Blog_site/settings.py
@@ -117,11 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-
+import os 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-import os 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
